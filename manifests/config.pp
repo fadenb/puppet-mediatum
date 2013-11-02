3be3203580c635629c0d027342fe620493706f30
Tristan Helmich <tristan.helmich@gmail.com>
2013-11-02 10:43:39 +0100

    ensure configured direcotries DO exist

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,9 +6,13 @@ class mediatum::config (
   $config_group   = $mediatum::params::config_group,
   $config_mode    = $mediatum::params::config_mode,
 
-  $cfg_hostname   = $hostname,
-  $cfg_port       = $mediatum::params::cfg_port,
-
+  $cfg_hostname         = $hostname,
+  $cfg_port             = $mediatum::params::cfg_port,
+  $cfg_base_dir         = $mediatum::params::cfg_base_dir,
+  $cfg_data_dir         = $mediatum::params::cfg_data_dir,
+  $cfg_searchstore_dir  = $mediatum::params::cfg_searchstore_dir,
+  $cfg_temp_dir         = $mediatum::params::cfg_temp_dir,
+  $cfg_log_dir          = $mediatum::params::cfg_log_dir,
 ) {
   anchor { 'mediatum::config::start': }
   anchor { 'mediatum::config::end': }
@@ -23,14 +27,24 @@ class mediatum::config (
     group   => $config_group,
     require => Anchor['mediatum::config::start'],
     before  => Anchor['mediatum::config::end'],
-
   }
+
   file { $config_file:
     ensure  => $config_state,
     owner   => $config_owner,
     group   => $config_group,
     mode    => $config_mode,
     content => template('mediatum/mediatum.cfg.erb'),
+    require => Anchor['mediatum::config::start'],
+    before  => Anchor['mediatum::config::end'],
+  }
+
+  # make sure configured folders do exist
+  file { [$cfg_base_dir, $cfg_data_dir, $cfg_searchstore_dir, $cfg_temp_dir,
+          $cfg_log_dir, ]:
+    ensure  => 'directory',
+    owner   => $config_owner,
+    group   => $config_group,
     require => Anchor['mediatum::config::start'],
     before  => Anchor['mediatum::config::end'],
   }
