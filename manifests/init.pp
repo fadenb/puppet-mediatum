29cc2867b620aa69e8d9b5354a305d056d2f5437
Tristan Helmich <tristan.helmich@gmail.com>
2013-11-25 21:21:03 +0100

    initial fixtures support

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
-class mediatum inherits params {
+class mediatum inherits mediatum::params {
   anchor { 'mediatum::start': }   ->
   class { 'mediatum::package': }  ->
+  class { 'mediatum::fixtures': } ->
   class { 'mediatum::config': }   ~>
   class { 'mediatum::service': }  ->
   anchor { 'mediatum::end': }
