1c569ebcfeb2aaa8a7b0d9bf7dfe52f7757e0306
Tristan Helmich <tristan.helmich@gmail.com>
2013-12-01 11:40:44 +0100

    fix run without fixtures

--- manifests/fixtures.pp
+++ manifests/fixtures.pp
@@ -10,25 +10,43 @@ class mediatum::fixtures (
 
   $cfg_data_dir = $mediatum::params::cfg_data_dir,
 ) {
-  anchor { 'mediatum::fixtures::start': } ->
-  # Get file and make it available locally to allow mysql client to read it
-  file { '/tmp/mediatum_fixtures.sql':
-    ensure  => present,
-    source  => $fixtures_sql_commands,
-  }                                       ->
-  exec { 'Import mediatum fixtures into database':
-    # we assume fixtures are for testing *only* If you use this on a production host prepare yourself to share your mysql credentials with every other user ;)
-    command => "/usr/bin/mysql --host=${cfg_db_host} --port=${cfg_db_port} --user=${cfg_db_user} --password=${cfg_db_pass} ${cfg_db_database} < /tmp/mediatum_fixtures.sql",
-  }                                       ->
-  # write archive contents into a file in order to extract them via archive::extract
-  file { '/tmp/mediatum_fixtures.tgz':
-    ensure  => present,
-    source  => $fixtures_file_archive,
-  }                                       ->
-  archive::extract { 'mediatum_fixtures':
-    target     => $cfg_data_dir,
-    src_target => '/tmp/',
-    extension  => 'tgz',
-  }                                       ->
+  anchor { 'mediatum::fixtures::start': }
   anchor { 'mediatum::fixtures::end': }
+
+  if $fixtures_sql_commands == '' {
+    info("${::calling_class}: No database fixtures to load")
+  } else {
+    # Get file and make it available locally to allow mysql client to read it
+    file { '/tmp/mediatum_fixtures.sql':
+      ensure  => present,
+      source  => $fixtures_sql_commands,
+      require => Anchor['mediatum::fixtures::start'],
+      before  => Anchor['mediatum::fixtures::end'],
+    }                                       ->
+    exec { 'Import mediatum fixtures into database':
+      # we assume fixtures are for testing *only* If you use this on a production host prepare yourself to share your mysql credentials with every other user ;)
+      command => "/usr/bin/mysql --host=${cfg_db_host} --port=${cfg_db_port} --user=${cfg_db_user} --password=${cfg_db_pass} ${cfg_db_database} < /tmp/mediatum_fixtures.sql",
+      require => Anchor['mediatum::fixtures::start'],
+      before  => Anchor['mediatum::fixtures::end'],
+    }
+  }
+
+  if $fixtures_file_archive == '' {
+    info("${::calling_class}: No file fixtures to load")
+  } else {
+    # write archive contents into a file in order to extract them via archive::extract
+    file { '/tmp/mediatum_fixtures.tgz':
+      ensure  => present,
+      source  => $fixtures_file_archive,
+      require => Anchor['mediatum::fixtures::start'],
+      before  => Anchor['mediatum::fixtures::end'],
+    }                                       ->
+    archive::extract { 'mediatum_fixtures':
+      target     => $cfg_data_dir,
+      src_target => '/tmp/',
+      extension  => 'tgz',
+      require    => Anchor['mediatum::fixtures::start'],
+      before     => Anchor['mediatum::fixtures::end'],
+    }
+  }
 }
